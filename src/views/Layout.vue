57321b6769fa3f03e64700f1db13ab3735b7a9bd
collie <2541474139@qq.com>
2023-09-13 01:00:23 +0800

    实现pc端menu-aside的隐藏与展示

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,7 +1,34 @@
 <script lang="ts" setup>
+import { ref, watchPostEffect, inject, Ref } from 'vue'
 import TopNav from '../components/TopNav.vue'
 import MenuContent from '../components/MenuContent.vue'
 
+const menuLocation = inject<Ref<boolean>>('menuLocation')
+const asideToggle = ref(true)
+const toggle = () => {
+  asideToggle.value = !asideToggle.value
+}
+const menuAside = ref<HTMLDivElement>(null)
+const mainContent = ref<HTMLDivElement>(null)
+const svgAsideToggle = ref<HTMLDivElement>(null)
+
+watchPostEffect(() => {
+  if (menuLocation.value) {
+    if (asideToggle.value === true) {
+      menuAside.value.style.left = '0'
+      mainContent.value.style.marginLeft = '200px'
+      svgAsideToggle.value.style.left = 'calc(100% - 12px)'
+      svgAsideToggle.value.style.transform = 'rotate(0deg)'
+    } else {
+      menuAside.value.style.left = '-200px'
+      mainContent.value.style.marginLeft = '0'
+      svgAsideToggle.value.style.left = 'calc(100% + 12px)'
+      svgAsideToggle.value.style.transform = 'rotate(180deg)'
+    }
+  } else {
+    mainContent.value.style.marginLeft = '0'
+  }
+})
 </script>
 
 
@@ -9,17 +36,23 @@ import MenuContent from '../components/MenuContent.vue'
   <div class="layout">
     <top-nav class="nav" />
     <div class="content">
-      <aside class="menu-aside">
+      <aside class="menu-aside" ref="menuAside" v-if="menuLocation">
         <menu-content />
+        <div class="aside-toggle-button" @click="toggle" ref="svgAsideToggle">
+          <svg class="icon">
+            <use xlink:href="#icon-left"></use>
+          </svg>
+        </div>
       </aside>
-      <main>
+      <main ref="mainContent">
         <router-view />
       </main>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
+
+<style lang="scss" scoped>
 @import "../assets/styles/common.scss";
 
 .layout {
@@ -46,6 +79,23 @@ import MenuContent from '../components/MenuContent.vue'
       top: 64px;
       transition: all 0.3s;
       padding: 8px;
+
+      .aside-toggle-button {
+        border: 1px solid $border-color;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+        position: absolute;
+        top: 260px;
+        left: calc(100% - 12px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #fff;
+        transform-origin: center;
+        transition: all 0.3s;
+      }
     }
 
     main {
@@ -54,7 +104,11 @@ import MenuContent from '../components/MenuContent.vue'
       padding: 32px 24px 56px 56px;
       margin-top: 64px;
       margin-left: 200px;
-
+      transition: all 0.3s;
+      @media (max-width: 500px) {
+        margin-left: 0;
+        padding-left: 24px;
+      }
     }
   }
 }
