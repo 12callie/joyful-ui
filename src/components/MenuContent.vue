234a14872933ecebd989577c89dbab8e6251822c
collie <2541474139@qq.com>
2023-09-13 00:10:26 +0800

    抽离出MenuContent

---
+++ src/components/MenuContent.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <ol class="menu-content">
+    <li>
+      <router-link to="/">首页</router-link>
+    </li>
+    <li class="menu-title">文档</li>
+    <li>
+      <router-link to="/joyful/doc/intro">介绍</router-link>
+    </li>
+    <li>
+      <router-link to="/joyful/doc/install">安装</router-link>
+    </li>
+    <li>
+      <router-link to="/joyful/doc/usage">在 SFC 中使用</router-link>
+    </li>
+    <li class="menu-title">组件</li>
+    <li>
+      <router-link to="/joyful/components/button">按钮 Button</router-link>
+    </li>
+    <li>
+      <router-link to="/joyful/components/switch">开关 Switch</router-link>
+    </li>
+  </ol>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/styles/common.scss";
+
+ol {
+  height: 100%;
+  overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  > li {
+    white-space: nowrap;
+    margin-bottom: 4px;
+
+    &.menu-title {
+      font-size: 20px;
+      font-weight: bold;
+      padding: 4px 18px;
+    }
+
+    > a {
+      display: block;
+      padding: 8px 28px;
+
+      &:hover {
+        background: #f3f3f5;
+      }
+
+      &.router-link-active {
+        color: $theme-color;
+        background: #e7f5ee;
+      }
+    }
+  }
+}
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import TopNav from '../components/TopNav.vue'
+import MenuContent from '../components/MenuContent.vue'
 
 </script>
 
@@ -9,28 +10,7 @@ import TopNav from '../components/TopNav.vue'
     <top-nav class="nav" />
     <div class="content">
       <aside class="menu-aside">
-        <ol class="menu-content">
-          <li>
-            <router-link to="/">首页</router-link>
-          </li>
-          <li class="menu-title">文档</li>
-          <li>
-            <router-link to="/joyful/doc/intro">介绍</router-link>
-          </li>
-          <li>
-            <router-link to="/joyful/doc/install">安装</router-link>
-          </li>
-          <li>
-            <router-link to="/joyful/doc/usage">在 SFC 中使用</router-link>
-          </li>
-          <li class="menu-title">组件</li>
-          <li>
-            <router-link to="/joyful/components/button">按钮 Button</router-link>
-          </li>
-          <li>
-            <router-link to="/joyful/components/switch">开关 Switch</router-link>
-          </li>
-        </ol>
+        <menu-content />
       </aside>
       <main>
         <router-view />
@@ -66,40 +46,6 @@ import TopNav from '../components/TopNav.vue'
       top: 64px;
       transition: all 0.3s;
       padding: 8px;
-
-      ol {
-        height: 100%;
-        overflow: scroll;
-
-        &::-webkit-scrollbar {
-          display: none;
-        }
-
-        > li {
-          white-space: nowrap;
-          margin-bottom: 4px;
-
-          &.menu-title {
-            font-size: 20px;
-            font-weight: bold;
-            padding: 4px 18px;
-          }
-
-          > a {
-            display: block;
-            padding: 8px 28px;
-
-            &:hover {
-              background: #f3f3f5;
-            }
-
-            &.router-link-active {
-              color: $theme-color;
-              background: #e7f5ee;
-            }
-          }
-        }
-      }
     }
 
     main {
