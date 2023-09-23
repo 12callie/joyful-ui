e55f314dba6e14a9161abadadcdd6d589b4bcf7b
collie <2541474139@qq.com>
2023-09-23 22:56:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git remote add origin git@github.com:12callie/joyful-website.git &&
 git branch -M main &&
 git push -f -u origin main &&
 cd ..
-echo https://12callie.github.io/gulu-ui-website/index.html
+echo https://12callie.github.io/joyful-website/index.html
