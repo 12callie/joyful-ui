022a95fb7b04341c2a635c0a9c55e7854bed2a2b
collie <2541474139@qq.com>
2023-09-23 20:51:28 +0800

    一键部署

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git remote add origin git@github.com:12callie/joyful-website.git &&
+git branch -M main &&
+git push -f -u origin main &&
+cd ..
+echo https://12callie.github.io/gulu-ui-website/index.html
